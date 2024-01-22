26ee3a81a359eeae2c3ccec617ada7719d8efd9a
Therese Cecilia Johansson <Theresececilias@gmail.com>
2024-01-22 09:48:35 +0100

    Use BlocConsumer

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -3,7 +3,9 @@ part of 'counter_cubit.dart';
 class CounterState {
   CounterState({
     required this.counterValue,
+    required this.wasIncremented,
   });
 
   int counterValue;
+  bool wasIncremented;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -42,40 +42,60 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  return Text(state.counterValue.toString());
-                },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: BlocConsumer<CounterCubit, CounterState>(
+                   listener: (context, state) {
+                    if (state.wasIncremented == true) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Incremented!'),
+                          duration: Duration(milliseconds: 300),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Decremented!"),
+                          duration: Duration(
+                            milliseconds: 300,
+                          ),
+                        ),
+                      );
+                    }
                   },
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
+                  builder: (context, state) {
+                    return Text(state.counterValue.toString());
                   },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
                 ),
-              ],
-            )
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    tooltip: 'Decrement',
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
